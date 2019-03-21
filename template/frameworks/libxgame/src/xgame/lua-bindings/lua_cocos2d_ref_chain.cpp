b2285a896c2da79dcb8038d5d844159771735d5c
O <codetypes@gmail.com>
2019-03-21 21:08:03 +0800

    优化director引用链

--- template/frameworks/libxgame/src/xgame/lua-bindings/lua_cocos2d_ref_chain.cpp
+++ template/frameworks/libxgame/src/xgame/lua-bindings/lua_cocos2d_ref_chain.cpp
@@ -109,11 +109,12 @@ static int should_unref_obj(lua_State *L)
     if (olua_isa(L, -2, "cc.Action")) {
         cocos2d::Action *obj = olua_touserdata(L, -2, cocos2d::Action *);
         if (obj) {
-            if (!obj->getTarget() || obj->isDone() || obj->getReferenceCount() == 1) {
+            unsigned int curr = obj->getReferenceCount();
+            if (!obj->getTarget() || obj->isDone() || curr == 1) {
                 return 1;
             } else if (olua_isinteger(L, -1)) {
-                int last = (int)olua_tointeger(L, -1);
-                if (obj->getReferenceCount() < last) {
+                unsigned int last = (unsigned int)olua_tointeger(L, -1);
+                if (curr < last) {
                     return 1;
                 }
             }
@@ -121,8 +122,9 @@ static int should_unref_obj(lua_State *L)
     } else if (olua_isa(L, -2, "cc.Ref")) {
         cocos2d::Ref *obj = (cocos2d::Ref *)olua_toobj(L, -2, "cc.Ref");
         if (obj && olua_isinteger(L, -1)) {
-            int last = (int)olua_tointeger(L, -1);
-            if (obj->getReferenceCount() < last) {
+            unsigned int last = (unsigned int)olua_tointeger(L, -1);
+            unsigned int curr = obj->getReferenceCount();
+            if (curr < last || curr == 1) {
                 return 1;
             }
         }
@@ -144,79 +146,36 @@ static int mapunef_by_compare(lua_State *L)
     return lua_gettop(L);
 }
 
-//
-// Director
-//
-static int wrap_cocos2d_Director_getRunningScene(lua_State *L)
+static int mapref_argument_value_and_mapunef_by_compare(lua_State *L)
 {
+    olua_mapref(L, 1, REFNAME, 2);
+    record_reference_count(L, 1);
     call_real_function(L, KEEP_SELF);
-    olua_arrayref(L, 1, REFNAME, 2);
+    check_reference_count(L, 1);
     lua_remove(L, 1);
     return lua_gettop(L);
 }
 
-// void runWithScene(Scene *scene)
-// void pushScene(Scene *scene)
-static int wrap_cocos2d_Director_pushScene(lua_State *L)
-{
-    olua_arrayref(L, 1, REFNAME, 2);
-    call_real_function(L, false);
-    return lua_gettop(L);
-}
-
-// void popScene()
-static int wrap_cocos2d_Director_popScene(lua_State *L)
-{
-    olua_arrayunref(L, 1, REFNAME, -1);
-    call_real_function(L, false);
-    return lua_gettop(L);
-}
-
-// void popToRootScene()
-static int wrap_cocos2d_Director_popToRootScene(lua_State *L)
-{
-    int len = (int)olua_arraylen(L, 1, REFNAME);
-    while (len > 1) {
-        olua_arrayunref(L, 1, REFNAME, len--);
-    }
-    call_real_function(L, false);
-    return lua_gettop(L);
-}
-
-// void popToSceneStackLevel(int level)
-static int wrap_cocos2d_Director_popToSceneStackLevel(lua_State *L)
-{
-    
-    int level = (int)olua_checkinteger(L, 2);
-    int len = (int)olua_arraylen(L, 1, REFNAME);
-    while (len > level) {
-        olua_arrayunref(L, 1, REFNAME, len--);
-    }
-    call_real_function(L, false);
-    return lua_gettop(L);
-}
-
-// void replaceScene(Scene *scene)
-static int wrap_cocos2d_Director_replaceScene(lua_State *L)
-{
-    
-    olua_arrayunref(L, 1, REFNAME, -1);
-    olua_arrayref(L, 1, REFNAME, 2);
-    call_real_function(L, false);
-    return lua_gettop(L);
-}
-
+//
+// Director
+//
 static int wrap_cocos2d_Director(lua_State *L)
 {
     luaL_getmetatable(L, "cc.Director");
     CCASSERT(olua_istable(L, -1), "not found 'cc.Director'");
-    wrap_getf(L, DIRECTOR_SCENES, "runningScene", "getRunningScene", wrap_cocos2d_Director_getRunningScene);
-    wrap_func(L, DIRECTOR_SCENES, "runWithScene", wrap_cocos2d_Director_pushScene);
-    wrap_func(L, DIRECTOR_SCENES, "pushScene", wrap_cocos2d_Director_pushScene);
-    wrap_func(L, DIRECTOR_SCENES, "popScene", wrap_cocos2d_Director_popScene);
-    wrap_func(L, DIRECTOR_SCENES, "popToRootScene", wrap_cocos2d_Director_popToRootScene);
-    wrap_func(L, DIRECTOR_SCENES, "popToSceneStackLevel", wrap_cocos2d_Director_popToSceneStackLevel);
-    wrap_func(L, DIRECTOR_SCENES, "replaceScene", wrap_cocos2d_Director_replaceScene);
+    // void runWithScene(Scene *scene)
+    // void pushScene(Scene *scene)
+    // void popScene()
+    // void popToRootScene()
+    // void popToSceneStackLevel(int level)
+    // void replaceScene(Scene *scene)
+    wrap_getf(L, DIRECTOR_SCENES, "runningScene", "getRunningScene", mapref_return_value);
+    wrap_func(L, DIRECTOR_SCENES, "runWithScene", mapref_argument_value_and_mapunef_by_compare);
+    wrap_func(L, DIRECTOR_SCENES, "pushScene", mapref_argument_value);
+    wrap_func(L, DIRECTOR_SCENES, "popScene", mapunef_by_compare);
+    wrap_func(L, DIRECTOR_SCENES, "popToRootScene", mapunef_by_compare);
+    wrap_func(L, DIRECTOR_SCENES, "popToSceneStackLevel", mapunef_by_compare);
+    wrap_func(L, DIRECTOR_SCENES, "replaceScene", mapref_argument_value_and_mapunef_by_compare);
     return 0;
 }
 
@@ -322,16 +281,6 @@ static int wrap_cocos2d_Node_removeChildByName(lua_State *L)
     return lua_gettop(L);
 }
 
-// Action* runAction(Action* action)
-static int wrap_cocos2d_Node_runAction(lua_State *L)
-{
-    record_reference_count(L, 1);
-    check_reference_count(L, 1);
-    olua_mapref(L, 1, REFNAME, 2);
-    call_real_function(L, false);
-    return lua_gettop(L);
-}
-
 // bool removeComponent(const std::string& name)
 // bool removeComponent(Component *component)
 static int wrap_cocos2d_Node_removeComponent(lua_State *L)
@@ -375,12 +324,13 @@ static int wrap_cocos2d_Node(lua_State *L)
     wrap_func(L, NODE_CHILDREN, "removeAllChildren", mapunrefall);
     wrap_func(L, NODE_CHILDREN, "removeAllChildrenWithCleanup", mapunrefall);
     
-    wrap_func(L, NODE_ACTIONS, "runAction", wrap_cocos2d_Node_runAction);
+    // Action* runAction(Action* action)
     // void stopAllActions()
     // void stopAction(Action* action)
     // void stopActionByTag(int tag)
     // void stopAllActionsByTag(int tag)
     // void stopActionsByFlags(unsigned int flags)
+    wrap_func(L, NODE_ACTIONS, "runAction", mapref_argument_value_and_mapunef_by_compare);
     wrap_func(L, NODE_ACTIONS, "stopAllActions", mapunef_by_compare);
     wrap_func(L, NODE_ACTIONS, "stopAction", mapunef_by_compare);
     wrap_func(L, NODE_ACTIONS, "stopActionByTag", mapunef_by_compare);

56dec9030c7c31ad6895f33ee38fe1fadd31bb88
O <codetypes@gmail.com>
2019-04-24 12:15:54 +0800

    不使用BuglyAgent

--- frameworks/libxgame/src/bugly/CrashReport-ios.mm
+++ frameworks/libxgame/src/bugly/CrashReport-ios.mm
@@ -1,9 +1,7 @@
+#import "CrashReport.h"
 
-#include "CrashReport.h"
-#include "BuglyAgent.h"
-
-#include "cocos2d.h"
-#include <string.h>
+#import "cocos2d.h"
+#import <string.h>
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
 
@@ -15,6 +13,7 @@
 #endif
 
 #import <Foundation/Foundation.h>
+#import <Bugly/Bugly.h>
 
 #define NSStringMake(const_char_pointer) (const_char_pointer == NULL ? nil : @(const_char_pointer))
 #define NSStringMakeNonnull(const_char_pointer) (const_char_pointer == NULL ? @"" : @(const_char_pointer))
@@ -23,6 +22,9 @@
 #define LOGI(fmt, args...) CCLOG("[Info] %s: " fmt, LOG_TAG, ##args)
 #define LOGW(fmt, args...) CCLOGERROR("[Warn] %s: " fmt, LOG_TAG, ##args)
 #define LOGE(fmt, args...) CCLOGERROR("[Error] %s: " fmt, LOG_TAG, ##args)
+
+const char *_channel = nullptr;
+const char *_version = nullptr;
 
 CrashReport::CrashReport(){
     LOGD("%s", __FUNCTION__);
@@ -37,45 +39,43 @@ void CrashReport::initCrashReport(const char* appId, bool isDebug) {
 }
 
 bool CrashReport::initialized = false;
-bool CrashReport::hasSetGameType = false;
 int CrashReport::crashReporterType = 0;
 
 void CrashReport::initCrashReport(const char* appId, bool isDebug, CrashReport::CRLogLevel level)
 {
     if (!initialized) {
         LOGI("Init the sdk with App ID: %s", appId);
-        [BuglyAgent initWithAppId: NSStringMake(appId) debugMode:isDebug logger:(BALogLevel)level];
+        BuglyConfig *config = [[BuglyConfig alloc] init];
+        config.channel = NSStringMake(_channel);
+        config.version = NSStringMake(_version);
+        [Bugly startWithAppId:NSStringMake(appId) developmentDevice:isDebug config:config];
         initialized = true;
     }
 }
 
 void CrashReport::setTag(int tag)
 {
-    setGameType();
     if (tag < 0) {
         return;
     }
-    [BuglyAgent setSceneTag:tag];
+    [Bugly setTag:tag];
     LOGI("Set user scene tag id: %d", tag);
 }
 
 void CrashReport::addUserValue(const char* key, const char* value)
 {
-    setGameType();
     LOGI("Set user Key-Value: [%s, %s]", key, value);
-    [BuglyAgent setSceneValue:NSStringMakeNonnull(value) forKey:NSStringMakeNonnull(key)];
+    [Bugly setUserValue:NSStringMakeNonnull(value) forKey:NSStringMakeNonnull(key)];
 }
 
 void CrashReport::removeUserValue(const char* key)
 {
-    setGameType();
-    [BuglyAgent removeSceneValueForKey:NSStringMakeNonnull(key)];
+    //[BuglyAgent removeSceneValueForKey:NSStringMakeNonnull(key)];
 }
 
 void CrashReport::setUserId(const char* userId)
 {
-    setGameType();
-    [BuglyAgent setUserIdentifier:NSStringMake(userId)];
+    [Bugly setUserIdentifier:NSStringMake(userId)];
     LOGI("Set user id: %s", userId);
 }
 
@@ -86,25 +86,23 @@ void CrashReport::reportException(int category, const char* type, const char* ms
 
 void CrashReport::reportException(int category, const char* type, const char* msg, const char* traceback, bool quit)
 {
-    [BuglyAgent reportException:category
-                           name:NSStringMake(type)
-                        message:NSStringMake(msg)
-                     stackTrace:NSStringMake(traceback)
-                       userInfo:nil
-                   terminateApp:quit ? YES : NO];
+    [Bugly reportExceptionWithCategory:category
+                                  name:NSStringMake(type)
+                                reason:NSStringMake(msg)
+                             callStack:[NSArray arrayWithObjects:NSStringMake(traceback), nil]
+                             extraInfo:@{}
+                          terminateApp:quit ? YES : NO];
 }
 
-void CrashReport::setAppChannel(const char * channel)
+void CrashReport::setAppChannel(const char *channel)
 {
-    setGameType();
-    [BuglyAgent setAppChannel:NSStringMake(channel)];
+    _channel = channel;
     LOGI("Set channel: %s", channel);
 }
 
-void CrashReport::setAppVersion(const char * version)
+void CrashReport::setAppVersion(const char *version)
 {
-    setGameType();
-    [BuglyAgent setAppVersion:NSStringMake(version)];
+    _version = version;
     LOGI("Set version: %s", version);
 }
 
@@ -125,21 +123,16 @@ void CrashReport::log(CrashReport::CRLogLevel level, const char * tag, const cha
         LOGW("The length[%d] of string is out of the buffer size[%d]", size, LOG_BUFFER_SIZE);
     }
     
-    NSInteger pLevel = (level >= 4 ? 1 : (level == 3 ? 2 : (level == 2 ? 4 : (level == 1 ? 8 : (level == 0 ? 16 : 0)))));
-    [BuglyAgent level:(BALogLevel)pLevel tag:NSStringMake(tag) log:NSStringMake(msg)];
+    [BuglyLog level:(BuglyLogLevel)level tag:NSStringMake(tag) log:@"%s", msg];
 }
 
 void CrashReport::setGameType()
 {
-    if (hasSetGameType) {
-        return;
-    }
 }
 
 void CrashReport::setCrashReporterType(int type)
 {
     crashReporterType = type;
-    [BuglyAgent configCrashReporterType:(CrashReporterType)type];
     LOGI("Config crash reporter type: %d", type);
 }
 

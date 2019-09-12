b58f2795f8de5a9ccee5d8a86a8ca3256704ca5c
O <codetypes@gmail.com>
2019-09-12 10:39:33 +0800

    update spine and spine test

--- assets/src/test/SpineTest.lua
+++ assets/src/test/SpineTest.lua
@@ -7,7 +7,7 @@ local SkeletonAnimation = require "sp.SkeletonAnimation"
 local SkeletonData = require "sp.SkeletonData"
 
 function SpineTest:ctor()
-    local data = SkeletonData.new("res/spine/raptor.json", "res/spine/raptor.atlas", 0.4)
+    local data = SkeletonData.new("res/spine/raptor-pro.skel", "res/spine/raptor-pro.atlas", 0.4)
     local animation = SkeletonAnimation.createWithData(data)
     animation.x = 500
     animation.y = 100

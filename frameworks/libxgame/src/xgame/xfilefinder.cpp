dee2ad30ea32f49ac5acec58badc415193f60c52
O <codetypes@gmail.com>
2019-12-13 10:24:04 +0800

    pack builtin assets on iOS

--- frameworks/libxgame/src/xgame/xfilefinder.cpp
+++ frameworks/libxgame/src/xgame/xfilefinder.cpp
@@ -3,9 +3,15 @@
 
 NS_XGAME_BEGIN
 
+FileFinder *FileFinder::create()
+{
+    auto fs = new FileFinder();
+    fs->init();
+    return fs;
+}
+
 FileFinder::FileFinder()
 {
-    init();
 #if CC_TARGET_PLATFORM == CC_PLATFORM_MAC
     std::string path = filesystem::getAppDataDirectory();
     setWritablePath(path + "/");
@@ -16,9 +22,37 @@ FileFinder::~FileFinder()
 {
 }
 
-std::string FileFinder::fullPathForFilename(const std::string &filename) const
+bool FileFinder::init()
 {
-    return FileUtils::fullPathForFilename(filename);
+    SuperFileUtils::init();
+    
+    _builtinAssets.init("builtin.assets");
+    
+    return true;
+}
+
+cocos2d::FileUtils::Status FileFinder::getContents(const std::string& filename, cocos2d::ResizableBuffer* buffer) const
+{
+    if (SuperFileUtils::getContents(filename, buffer) == FileUtils::Status::OK) {
+        return FileUtils::Status::OK;
+    } else {
+        return _builtinAssets.getContents(filename, buffer);
+    }
+}
+
+std::string FileFinder::getFullPathForFilenameWithinDirectory(const std::string& directory, const std::string& filename) const
+{
+    std::string fullPath = SuperFileUtils::getFullPathForFilenameWithinDirectory(directory, filename);
+    if (fullPath.size() == 0) {
+        fullPath.append(directory).append(filename);
+        if (_builtinAssets.exist(fullPath)) {
+            return fullPath;
+        } else {
+            return "";
+        }
+    } else {
+        return fullPath;
+    }
 }
 
 NS_XGAME_END

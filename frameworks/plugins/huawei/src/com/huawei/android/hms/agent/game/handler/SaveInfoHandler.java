b9e24a5ee06fe84262241fa9081fc2f67aaa4717
O <codetypes@gmail.com>
2019-12-31 10:51:35 +0800

    [android] update huawei sdk

--- frameworks/plugins/huawei/src/com/huawei/android/hms/agent/game/handler/SaveInfoHandler.java
+++
@@ -1,9 +0,0 @@
-package com.huawei.android.hms.agent.game.handler;
-
-import com.huawei.android.hms.agent.common.handler.ICallbackCode;
-
-/**
- * 保存玩家信息请求结果回调
- */
-public interface SaveInfoHandler extends ICallbackCode {
-}

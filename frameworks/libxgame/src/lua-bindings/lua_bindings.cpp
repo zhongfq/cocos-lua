5926514300f8fbc9d28dd1c97d05dfe18d5b99c2
sishui <sishuizh@gmail.com>
2020-01-10 00:33:59 +0800

    add module switch, default ON

--- frameworks/libxgame/src/lua-bindings/lua_bindings.cpp
+++ frameworks/libxgame/src/lua-bindings/lua_bindings.cpp
@@ -12,37 +12,44 @@
 #include "lua_iap.h"
 #include "lua_recorder.h"
 #include "lua_keychain.h"
-#ifdef USE_LUA_SOCKET
-	#include "lua_socket.h"
-	#include "luasocket/luasocket.h"
-	#include "luasocket/luasocket_scripts.h"
-	#include "luasocket/mime.h"
-#endif // USE_LUA_SOCKET
 #include "xgame/xruntime.h"
 #include "xgame/xlua.h"
 #include "md5/md5.h"
 
+#ifdef USE_LUA_SOCKET
+    #include "lua_socket.h"
+    #include "luasocket/luasocket.h"
+    #include "luasocket/luasocket_scripts.h"
+    #include "luasocket/mime.h"
+#endif // USE_LUA_SOCKET
+
 #ifdef USE_BUGLY
-	#include "bugly/lua_bugly.h"
+    #include "bugly/lua_bugly.h"
 #endif //USE_BUGLY
 
 #ifdef USE_CJSON
-	#include "cjson/lua_cjson.h"
+    #include "cjson/lua_cjson.h"
 #endif //USE_CJSON
 
 #ifdef USE_PBC
-	#include "pbc/pbc.h"
+    #include "pbc/pbc.h"
 #endif // USE_PBC
-#include "lame/lua_lame.h"
-#include "qrencode/lua_qrcode.h"
 
 #ifdef USE_SPROTO
-	#include "sproto/lsproto.h"
+    #include "sproto/lsproto.h"
 #endif // USE_SPROTO
 
 #ifdef USE_LPEG
-	#include "lpeg/lptree.h"
+    #include "lpeg/lptree.h"
 #endif //USE_LPEG
+
+#ifdef USE_LAME
+    #include "lame/lua_lame.h"
+#endif
+
+#ifdef USE_QRCODE
+    #include "qrencode/lua_qrcode.h"
+#endif //USE_QRCODE
 
 #ifndef _WIN32
 #include "libpdebug.h"
@@ -57,49 +64,56 @@ int luaopen_bindings(lua_State *L)
     olua_dofunc(L, luaopen_cocos2d_ui);
     olua_dofunc(L, luaopen_xgame);
     olua_dofunc(L, luaopen_xml_http_request);
-    
+
 #ifndef _WIN32
     //pdebug_init(L);
 #endif
-    
-#ifdef USE_LUA_SOCKET
-	olua_require(L, "socket.core", luaopen_socket_core);
-	olua_require(L, "mime.core", luaopen_mime_core);
-	olua_require(L, "luasocket.scripts", luaopen_luasocket_scripts);
-	olua_require(L, "kernel.Socket", luaopen_socket);
-#endif // USE_LUA_SOCKET
-    
+
     olua_require(L, "olua", luaopen_olua);
+#ifdef USE_LUA_SOCKET
+    olua_require(L, "socket.core", luaopen_socket_core);
+    olua_require(L, "mime.core", luaopen_mime_core);
+    olua_require(L, "luasocket.scripts", luaopen_luasocket_scripts);
+    olua_require(L, "kernel.Socket", luaopen_socket);
+#endif // USE_LUA_SOCKET
+
 #ifdef USE_SPROTO
-	olua_require(L, "sproto.core", luaopen_sproto_core);
+    olua_require(L, "sproto.core", luaopen_sproto_core);
 #endif //USE_SPROTO
 
 #ifdef USE_LPEG
-	olua_require(L, "lpeg", luaopen_lpeg);
+    olua_require(L, "lpeg", luaopen_lpeg);
 #endif // USE_LPEG
 
-
 #ifdef USE_PBC
-	olua_require(L, "protobuf.c", luaopen_protobuf_c);
+    olua_require(L, "protobuf.c", luaopen_protobuf_c);
 #endif // USE_PBC
+
+#ifdef USE_CJSON
+    olua_require(L, "cjson", luaopen_cjson);
+    olua_require(L, "cjson.safe", luaopen_cjson_safe);
+#endif //USE_CJSON
 
     olua_require(L, "base64", luaopen_base64);
     olua_require(L, "xxtea", luaopen_xxtea);
     olua_require(L, "openssl", luaopen_openssl);
-#ifdef USE_CJSON
-	olua_require(L, "cjson", luaopen_cjson);
-	olua_require(L, "cjson.safe", luaopen_cjson_safe);
-#endif //USE_CJSON
     olua_require(L, "md5" , luaopen_md5);
+
+#ifdef USE_QRCODE
     olua_require(L, "kernel.qrcode", luaopen_qrcode);
     olua_require(L, "kernel.QRSprite", luaopen_qrsprite);
-    olua_require(L, "kernel.luaj", luaopen_javabridge);
+#endif //USE_QRCODE
+
+#ifdef USE_LAME
     olua_require(L, "kernel.lame", luaopen_lame);
-    olua_require(L, "kernel.keychain", luaopen_keychain);
+#endif //USE_LAME
+
 #ifdef USE_BUGLY
-	olua_require(L, "kernel.bugly", luaopen_bugly);
+    olua_require(L, "kernel.bugly", luaopen_bugly);
 #endif // USE_BUGLY
 
+    olua_require(L, "kernel.luaj", luaopen_javabridge);
+    olua_require(L, "kernel.keychain", luaopen_keychain);
     olua_require(L, "kernel.plugins.photo", luaopen_photo);
     olua_require(L, "kernel.plugins.recorder", luaopen_recorder);
     olua_require(L, "kernel.plugins.iap", luaopen_iap);

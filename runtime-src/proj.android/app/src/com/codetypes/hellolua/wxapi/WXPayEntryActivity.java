42d43a077a6030712ede6411257bc9e80e5f75a4
O <codetypes@gmail.com>
2021-01-08 22:31:31 +0800

    fix alipay sdk

--- runtime-src/proj.android/app/src/com/codetypes/hellolua/wxapi/WXPayEntryActivity.java
+++ runtime-src/proj.android/app/src/com/codetypes/hellolua/wxapi/WXPayEntryActivity.java
@@ -1,6 +1,6 @@
 package com.codetypes.hellolua.wxapi;
 
-import kernel.plugins.wechat.WeChatPayEntryActivity;
+import kernel.plugin.wechat.WeChatPayEntryActivity;
 
 public class WXPayEntryActivity extends WeChatPayEntryActivity {
 }

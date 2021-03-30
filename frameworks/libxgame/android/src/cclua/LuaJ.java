19c16874998747396ffb27ca0262ad894fcbb855
O <codetypes@gmail.com>
2021-03-30 10:33:33 +0800

    the callback will be call after engine on resume

--- frameworks/libxgame/android/src/cclua/LuaJ.java
+++ frameworks/libxgame/android/src/cclua/LuaJ.java
@@ -4,26 +4,14 @@ public class LuaJ {
     private static native void call(int func, String value, boolean once);
 
     public static native void registerFeature(String api, boolean enabled);
+
     public static native void dispatchEvent(String event, String args);
 
     public static void invokeOnce(final int func, final String value) {
-        AppContext context = (AppContext) AppContext.getContext();
-        context.runOnGLThread(new Runnable() {
-            @Override
-            public void run() {
-                LuaJ.call(func, value, true);
-            }
-        });
+        LuaJ.call(func, value, true);
     }
 
-    public static void invoke(final int func, final String value)
-    {
-        AppContext context = (AppContext) AppContext.getContext();
-        context.runOnGLThread(new Runnable() {
-            @Override
-            public void run() {
-                LuaJ.call(func, value, false);
-            }
-        });
+    public static void invoke(final int func, final String value) {
+        LuaJ.call(func, value, false);
     }
 }
